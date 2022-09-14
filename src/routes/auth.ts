0a4407e6dccc8426edc589265f89d57afefef6fc
Tam Pham <tam.pham@blackbook.ai>
2022-09-14 14:36:32 +1000

    Set up all routes placeholders

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,9 +2,6 @@ import { Router, Request, Response } from 'express';
 
 const router = Router();
 
-/**
- * Handle login requests.
- */
 router.post('/login', async (req: Request, res: Response) => {
     res.send({});
 });
--- src/routes/singleView.ts
+++ src/routes/singleView.ts
@@ -2,7 +2,35 @@ import { Router, Request, Response } from 'express';
 
 const router = Router();
 
-router.post('/hikes', async (req: Request, res: Response) => {
+router.get('/hike/:id', async (req: Request, res: Response) => {
+    res.send({});
+});
+
+router.post('/:memoId/notes', async (req: Request, res: Response) => {
+    res.send({});
+});
+
+router.delete('/:memoId/notes', async (req: Request, res: Response) => {
+    res.send({});``
+});
+
+router.post('/:memoId/audio', async (req: Request, res: Response) => {
+    res.send({});
+});
+
+router.delete('/:memoId/audio', async (req: Request, res: Response) => {
+    res.send({});
+});
+
+router.post('/:memoId/image', async (req: Request, res: Response) => {
+    res.send({});
+});
+
+router.delete('/:memoId/image', async (req: Request, res: Response) => {
+    res.send({});
+});
+
+router.post('/:memoId/transcription', async (req: Request, res: Response) => {
     res.send({});
 });
 
