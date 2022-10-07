104c74aa9012238761991668222e8e1ce441c6d7
Tam Pham <tam.minhpham@outlook.com>
2022-10-07 15:19:09 +1000

    Allow all CORS headers

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -5,8 +5,7 @@ export const cors = (req: Request, res: Response, next: NextFunction) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
     res.header(
-        'Access-Control-Allow-Headers',
-        'Content-Type, Authorization, Content-Length, X-Requested-With'
+        'Access-Control-Allow-Headers', '*'
     );
 
     // Allow all options requests
